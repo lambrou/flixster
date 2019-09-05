013dd397fab8ae65792db05f4df820178841c3d5
lambrou <alexanderlambrou0602@gmail.com>
2019-09-05 18:04:59 +0000

    Added payments via stripe

--- Gemfile
+++ Gemfile
@@ -41,6 +41,7 @@ gem 'figaro'
 gem 'fog-aws'
 gem 'mini_magick'
 gem 'simple_form'
+gem 'stripe'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
---
+++ app/controllers/enrollments_controller.rb
@@ -0,0 +1,35 @@
+class EnrollmentsController < ApplicationController
+  before_action :authenticate_user!
+  
+  def new
+  end
+
+  def create
+  	# Amount in cents
+    @amount = 500
+
+    customer = Stripe::Customer.create({
+      email: params[:stripeEmail],
+      source: params[:stripeToken],
+    })
+
+    charge = Stripe::Charge.create({
+      customer: customer.id,
+      amount: @amount,
+      description: 'Rails Stripe customer',
+      currency: 'usd',
+    })
+
+    rescue Stripe::CardError => e
+      flash[:error] = e.message
+      redirect_to new_charge_path
+    end
+    current_user.enrollments.create(course: current_course)
+    redirect_to course_path(current_course)
+  end
+
+  private
+
+  def current_course
+    @current_course ||= Course.find(params[:course_id])
+  end
