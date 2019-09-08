92557ed08f5101389197c4e884ad809f1169469c
lambrou <alexanderlambrou0602@gmail.com>
2019-09-08 23:57:14 +0000

    Fixed error with adding a lesson

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,9 +1,11 @@
 class CoursesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+  
   def index
     @courses = Course.all
   end
 
-  def show
+   def show
     @course = Course.find(params[:id])
   end
 end
--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,6 +1,6 @@
 class Lesson < ApplicationRecord
-  belongs_to :section
   mount_uploader :video, VideoUploader
+  belongs_to :section
 
   include RankedModel
   ranks :row_order, with_same: :section_id
@@ -12,5 +12,12 @@ class Lesson < ApplicationRecord
     end
     return lesson
   end
-
+  
+  def previous_lesson
+      lesson = section.lessons.where("row_order < ?", self.row_order).rank(:row_order).last
+      if lesson.blank? && section.previous_section
+          return section.previous_section.lessons.rank(:row_order).last
+      end
+      return lesson
+    end
 end
